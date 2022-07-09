2b880981a16cece4764f26c90276a93495a1a0c2
diogoizzo <izzoadv@gmail.com>
2022-07-09 16:22:01 -0300

    Tentanto corrigir problema de CORS adicionando o origin na URL sem barra no final e habilitando a js api - headeres configurados para Access-Control-Allow-Origin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,34 @@
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        images: {
+            unoptimized: true,
+        },
+    },
     images: {
-        domains: ["source.unsplash.com/random/"],
+        loader: "custom",
+    },
+    async headers() {
+        return [
+            {
+                source: "/:paths*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "PUT, POST, PATCH, DELETE, GET",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CRSF-Token",
+                    },
+                ],
+            },
+        ];
     },
 };
 
