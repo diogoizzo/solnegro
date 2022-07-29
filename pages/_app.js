9e5190174a60d0c0de75347ec9beb415411c7d7f
diogoizzo <izzoadv@gmail.com>
2022-07-29 13:21:33 -0300

    Inserção de meta tags e favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Sol Negro</title>
+                <meta name="author" content="Diogo Izzo" />
+                <meta
+                    name="description"
+                    content="O curso que irá devastar suas crenças"
+                />
+                <meta
+                    name="keywords"
+                    content="misticismo, alemanhã nazista, hitler, curso histórico, verdades, hitler não morreu"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
