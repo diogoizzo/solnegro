b9c53a549064cc9174bc8cef80b6c3efa824ca12
diogoizzo <izzoadv@gmail.com>
2022-08-01 19:35:07 -0300

    Incluindo Gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module/dist/TagManager";
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        TagManager.initialize({ gtmId: "GTM-K42BDVS" });
+    }, []);
     return (
         <>
             <Head>
